d8ae689e424f72d9661da9b9645305b8ea43d758
Жахангир Таиров <jahangirtairov123@gmail.com>
2023-05-20 20:57:11 +0600

    Added mvp pattern template

---
+++ MVP Pattern Template.xctemplate/___FILEBASENAME___Router.swift
@@ -0,0 +1,14 @@
+//
+//  ___FILEHEADER___
+//
+
+import Foundation
+
+struct ___VARIABLE_productName:identifier___Router : ___VARIABLE_productName:identifier___RouterInput {
+    
+    private weak var viewController: ___VARIABLE_productName:identifier___ViewInput?
+    
+    func createModule() -> ___VARIABLE_productName:identifier___ViewInput {
+        
+    }
+}
